97979ede5f18259ed1561907d1730c09d0779814
Ani <aniruddhyagoswami6@gmail.com>
2025-07-02 00:38:37 +0530

    add all

--- agri/urls.py
+++ agri/urls.py
@@ -1,4 +1,3 @@
-# project/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -6,5 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portal.urls')),  # This includes the URLs from the portal app
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serving media files in development
+    path('', include('portal.urls')),  # Includes your portal app URLs
+]
+
+# Serve media files only in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
