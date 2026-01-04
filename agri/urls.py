0334d5079b88824f6a7da26c87b167f5d1d8ff11
Ani <aniruddhyagoswami6@gmail.com>
2026-01-04 13:35:50 +0530

    fina; blow

--- agri/urls.py
+++ agri/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
 
     # 3. Add the Sitemap URL here
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 
 if settings.DEBUG:
